ffe805728b92cb4c0618cab4666df7bb0ea22ad3
Tharushika251 <tharushikarukshani10@gmail.com>
2025-09-02 13:48:23 +0530

    updated server

--- server/server.js
+++ server/server.js
@@ -16,12 +16,11 @@ app.use(cors());
 app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is running'));
-app.use('/api/inngest', serve({ client: inngest, functions }))
-app.use('/api/user', userRouter)
+app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/user', userRouter);
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
 
 export default app;
 
